135980861a5caf317e759656ffb61567006bf101
infrit-webdev <104186919+infrit-webdev@users.noreply.github.com>
2024-05-22 00:21:54 -0700

    first porfolio commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
